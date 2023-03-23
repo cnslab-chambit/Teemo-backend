db8ad4593e9a0fbbc707cfe6e391904f91fcd8ce
yunshin <50646145+TransparentDeveloper@users.noreply.github.com>
2023-03-23 21:11:58 +0900

    #2 - Chatting Server 초기 설정 하였습니다.
    앞으로 Chatting Server 관련 수정, 테스트 등이 이 branch를 기반으로 진행하겠습니다.

---
+++ src/main/java/webSocket/TeemoBackendApplication.java
@@ -0,0 +1,13 @@
+package webSocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeemoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeemoBackendApplication.class, args);
+	}
+
+}
