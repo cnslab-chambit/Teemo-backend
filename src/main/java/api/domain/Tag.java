178b6b7c073234762e13931b4fbc37d249409dc7
YunShin <2younsin@gmail.com>
2023-03-28 14:27:40 +0900

    [Feat:#13] Tag Entity 필드 추가

--- src/main/java/api/domain/Tag.java
+++ src/main/java/api/domain/Tag.java
@@ -19,7 +19,18 @@ public class Tag {
     @JoinColumn(name = "member_id")
     private Member host;
 
+    // 모집 정보
+    private String title;   // 타이틀
+    private String detail;  // 상세설명
     private int limit;
+    private Gender targetGender; // 모집 성별
+    private Integer targetAgeUpper;  // 모집 나이 상한
+    private Integer targetAgeLower; //모집 나이 하한
+    private LocalDateTime createAt; // tag 생성 시간
+
+    // 위치 정보
+    private double latitude; // 위도
+    private double longitude; // 경도
 
     // Chatroom 을 관리 (1:Many)
     // Tag 가 사라지면 관련 Chatroom 도 싹 정리
@@ -30,8 +41,27 @@ public class Tag {
     public Tag() {}
 
     //==(위치 + 모집) 정보를 받을 생성자==//
-    public Tag(int limit) {
+    public Tag(
+            String title,
+            String detail,
+            int limit,
+            Gender targetGender,
+            Integer targetAgeUpper,
+            Integer targetAgeLower,
+            double latitude,
+            double longitude
+            )
+    {
+        this.title = title;
+        this.detail = detail;
         this.limit = limit;
+        this.targetGender = targetGender;
+        this.targetAgeUpper = targetAgeUpper;
+        this.targetAgeLower = targetAgeLower;
+        this.latitude = latitude;
+        this.longitude = longitude;
+
+        this.createAt = LocalDateTime.now();
     }
 
     //==연관관계 메서드==//
@@ -44,7 +74,6 @@ public class Tag {
     public void addChatRoom(Chatroom chatroom){
         /**
         * 추후에 limit 이용해서 채팅방 객수 조절
-
          */
 
         this.chatrooms.add(chatroom);
