9ca8e6345e4414b97f8b8b833de5d388c01c8809
YunShin <2younsin@gmail.com>
2023-04-01 19:35:59 +0900

    [Feat:#12]Member Entity 관련 도메인 로직 작성
    "회원 생성" 기능 추가

--- src/main/java/api/controller/MemberController.java
+++ src/main/java/api/controller/MemberController.java
@@ -2,10 +2,13 @@ package api.controller;
 
 import api.domain.Member;
 import api.domain.Role;
+import api.domain.dtos.CreateMemberRequest;
 import api.service.MemberService;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,24 +19,22 @@ public class MemberController {
     private final MemberService memberService;
 
     /**
-     * member 생성
+     * 회원 생성
      */
-    @PostMapping("/member/add")
-    public CreateMemberResponse createMember(@RequestBody CreateMemberRequest request){
-        Member member = new Member(request.getNickname(),request.getRole());
+    @PostMapping("/members")
+    public ResponseEntity createMember(@RequestBody CreateMemberRequest request){
+        Member member = new Member(
+                request.getEmail(),
+                request.getPassword(),
+                request.getNickname(),
+                request.getBirthday(),
+                request.getRole(),
+                request.getGender()
+        );
+        // 추후 중복 Email 확인
         Long memberId = memberService.join(member);
-        return new CreateMemberResponse(memberId);
+        return new ResponseEntity(HttpStatus.CREATED);
     }
 
 
-    @Data
-    @AllArgsConstructor
-    private static class CreateMemberResponse{
-        private Long memberId;
-    }
-    @Data
-    private static class CreateMemberRequest {
-        private String nickname;
-        private Role role;
-    }
 }
