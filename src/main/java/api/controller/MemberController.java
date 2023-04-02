c772e160f4e153ad9ba11f7f71b853bdbe22f908
YunShin <2younsin@gmail.com>
2023-04-02 17:44:40 +0900

    [Feat:#12]Member Entity 관련 도메인 로직 작성
    "자기 정보 조회" 기능 추가

--- src/main/java/api/controller/MemberController.java
+++ src/main/java/api/controller/MemberController.java
@@ -3,15 +3,14 @@ package api.controller;
 import api.domain.Member;
 import api.domain.Role;
 import api.domain.dtos.CreateMemberRequest;
+import api.domain.dtos.FindMemberResponse;
 import api.service.MemberService;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,6 +19,7 @@ public class MemberController {
 
     /**
      * 회원 생성
+     * 회원을 생성하고 회원 ID를 반환하는 엔드포인트
      */
     @PostMapping("/members")
     public ResponseEntity createMember(@RequestBody CreateMemberRequest request){
@@ -34,6 +34,15 @@ public class MemberController {
         // 추후 중복 Email 확인
         Long memberId = memberService.join(member);
         return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    /**
+     * 자기 정보 조회
+     */
+    @GetMapping("/members/{member_id}")
+    public FindMemberResponse findMemberById(@PathVariable("member_id")Long memberId){
+        Member member = memberService.findById(memberId);
+        return new FindMemberResponse(member);
     }
 
 
--- src/main/java/api/service/MemberService.java
+++ src/main/java/api/service/MemberService.java
@@ -7,14 +7,17 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional(readOnly = true)
+@Transactional(readOnly = false)
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
 
-    @Transactional
     public Long join(Member member){
         return memberRepository.save(member);
+    }
+
+    public Member findById(Long memberId){
+        return memberRepository.find(memberId);
     }
 
 }
