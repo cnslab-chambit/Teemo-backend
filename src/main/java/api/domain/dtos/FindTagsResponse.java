9ec783d083cbbc40a5c16648217539736c784e92
YunShin <2younsin@gmail.com>
2023-04-03 16:25:38 +0900

    [Refactor] FindTagsResponse DTO tagId 추가
    내 주변 tag 조회시, 특정 tag 조회를 위해 tagId를 함께 반환합니다.

--- src/main/java/api/domain/dtos/FindTagsResponse.java
+++ src/main/java/api/domain/dtos/FindTagsResponse.java
@@ -6,10 +6,12 @@ import lombok.Getter;
 
 @Getter
 public class FindTagsResponse {
+    private Long tagId; // 태그아이디
     private double latitude; // 위도
     private double longitude; // 경도
 
     public FindTagsResponse(Tag tag) {
+        this.tagId = tag.getId();
         this.latitude = tag.getLatitude();
         this.longitude = tag.getLongitude();
     }
