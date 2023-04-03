de44d8761b0fb1ad5886c897e962037dd5ebbf2f
YunShin <2younsin@gmail.com>
2023-04-03 20:33:23 +0900

    [Refactor] SubscribeTagResponse tagId 필드 추가

--- src/main/java/api/domain/dtos/SubscribeTagResponse.java
+++ src/main/java/api/domain/dtos/SubscribeTagResponse.java
@@ -5,10 +5,12 @@ import lombok.Data;
 
 @Data
 public class SubscribeTagResponse {
+    private Long tagId; // 태그아이디
     private double latitude; // tag 의 위도
     private double longitude; // tag 의 경도
 
     public SubscribeTagResponse(Tag tag){
+        this.tagId = tag.getId();
         this.latitude = tag.getLatitude();
         this.longitude = tag.getLongitude();
     }
