7009e23eadeced4b73a97d07544ed5184df0cbe9
YunShin <2younsin@gmail.com>
2023-04-04 20:31:12 +0900

    [Feat] 임의 데이터 추가

--- src/main/java/api/InitDB.java
+++ src/main/java/api/InitDB.java
@@ -117,8 +117,8 @@ public class InitDB {
             );
             em.persist(tag);
         }
-
         public void dbInit4() {
+            /** 호스트 전용 Data */
             Member member = new Member("test4@email.com",
                     "pass04",
                     "nickNam4",
@@ -126,18 +126,68 @@ public class InitDB {
                     Gender.MAN);
             em.persist(member);
 
+            /**
+             * 성별 상관 없음
+             * 나이 범위: 24 ~ 25
+             * 태그 생성 위치: (37.6190, 127.0597)
+             */
             Tag tag = new Tag(
                     "testTagTitle2",
                     "testTagDetail2",
                     3,
                     Gender.NOMATTER,
                     25,
-                    20,
+                    24,
                     37.61901,
                     127.05971,
                     member
             );
             em.persist(tag);
+        }
+        public void dbInit5() {
+            /** 호스트 전용 Data */
+            Member member = new Member("test5@email.com",
+                    "pass05",
+                    "nickNam5",
+                    LocalDate.of(2020, 10, 11),
+                    Gender.WOMAN);
+            em.persist(member);
+
+            /**
+             * 성별 상관 없음
+             * 나이 범위: 20 ~ 25
+             * 태그 생성 위치: (36.61901, 128.05971)
+             */
+            Tag tag = new Tag(
+                    "testTagTitle2",
+                    "testTagDetail2",
+                    5,
+                    Gender.NOMATTER,
+                    25,
+                    20,
+                    36.61901,
+                    128.05971,
+                    member
+            );
+            em.persist(tag);
+        }
+        public void dbInit6() {
+            /** 조회자 전용 Data */
+            Member member = new Member("test6@email.com",
+                    "pass06",
+                    "nickNam6",
+                    LocalDate.of(2000, 10, 11),
+                    Gender.WOMAN);
+            em.persist(member);
+        }
+        public void dbInit7() {
+            /** 게스트 전용 Data */
+            Member member = new Member("test7@email.com",
+                    "pass07",
+                    "nickNam7",
+                    LocalDate.of(1999, 10, 11),
+                    Gender.MAN);
+            em.persist(member);
         }
     }
 
