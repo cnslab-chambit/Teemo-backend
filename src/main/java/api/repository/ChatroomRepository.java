15bdecd5f04c19bfea0d66a513ab50a3842d7181
YunShin <2younsin@gmail.com>
2023-04-04 20:11:27 +0900

    [Feat] ChatroomRepository 에 chatroom 검색, 삭제 메서드 추가

--- src/main/java/api/repository/ChatroomRepository.java
+++ src/main/java/api/repository/ChatroomRepository.java
@@ -1,9 +1,12 @@
 package api.repository;
 
 import api.domain.Chatroom;
+import api.domain.Member;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class ChatroomRepository {
@@ -15,11 +18,31 @@ public class ChatroomRepository {
         return chatroom.getId();
     }
 
-    public Chatroom find(Long id){
-        return em.find(Chatroom.class, id);
+    public List<Chatroom> findHostChatroom(Long id)
+    {
+        /*
+         * 호스트의 id를 입력받아 관련 Chatroom List를 반환합니다.
+         */
+        return em.createQuery("select c from Chatroom c where c.host.id = :id", Chatroom.class)
+                .setParameter("id", id)
+                .getResultList();
     }
 
-    public void delete(Chatroom chatroom){
+    public Chatroom findGuestChatroom(Long id)
+    {
+        /*
+         * 게스트 id를 입력받아 관련 Chatroom 객체 을 반환합니다.
+         */
+        return em.createQuery("select c from Chatroom c where c.guest.id = :id", Chatroom.class)
+                .setParameter("id", id)
+                .getSingleResult();
+    }
+
+    public void remove(Chatroom chatroom){
+        /*
+         * 전달 받은 chatroom 객체를 db 상에서 삭제합니다.
+         */
         em.remove(chatroom);
     }
+
 }
