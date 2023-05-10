bbc39b47478e1762e3ba9eb0f4f1bbfb0f2ff51a
yunshin <50646145+TransparentDeveloper@users.noreply.github.com>
2023-05-10 14:56:57 +0900

    [Init] 초기설정

---
+++ src/main/java/Teemo/Teemo_backend/TeemoBackendApplication.java
@@ -0,0 +1,11 @@
+package Teemo.Teemo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeemoBackendApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TeemoBackendApplication.class, args);
+	}
+}
---
+++ src/test/java/Teemo/Teemo_backend/TeemoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package Teemo.Teemo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeemoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
