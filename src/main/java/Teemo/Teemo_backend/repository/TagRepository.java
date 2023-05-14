a898163e54626583854de89be7261d1f61708f6f
yunshin <2younsin@gmail.com>
2023-05-14 14:57:47 +0900

    [Feat] TagService interface -> method 추가
    추가대상: upload(),search(),find(),subscribe(),unsubscribe(),remove()

--- src/main/java/Teemo/Teemo_backend/repository/TagRepository.java
+++ src/main/java/Teemo/Teemo_backend/repository/TagRepository.java
@@ -6,8 +6,8 @@ import Teemo.Teemo_backend.domain.Tag;
 import java.util.List;
 
 public interface TagRepository {
-    Long save(); // 저장
-    Tag findById(); // id 로 검색
+    void save(Tag tag); // 저장
+    Tag findById(Long tagId); // id 로 검색
     List<Tag> findByCriteria(Double latitude, Double longitude, Integer age, Gender gender); // 조건에 맞춰 불러오기
     Long delete(Tag tag); // 삭제
 }
--- src/main/java/Teemo/Teemo_backend/service/TagService.java
+++ src/main/java/Teemo/Teemo_backend/service/TagService.java
@@ -1,4 +1,16 @@
 package Teemo.Teemo_backend.service;
 
+import Teemo.Teemo_backend.domain.Tag;
+import Teemo.Teemo_backend.domain.User;
+import Teemo.Teemo_backend.domain.dtos.*;
+
+import java.util.List;
+
 public interface TagService {
+    void upload(TagCreateRequest request); // 태그 업로드
+    List<Tag> search(Long userId, Double latitude, Double longitude); // 주변 태그 검색
+    TagFindResponse find(Long tagId); // 특정 태그 정보 검색
+    TagSubscribeResponse subscribe(Long userId, Long tagId); // 특정 태그 구독
+    void unsubscribe(Long userId, Long tagId);// 특정 태그 구독 취소
+    void remove(Long userId, Long tagId); // 태그 삭제
 }
