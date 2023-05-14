34a710fbad1556a17459cfb217e01d21a9b66555
yunshin <2younsin@gmail.com>
2023-05-14 14:50:15 +0900

    [Feat] Tag class -> users 멤버변수 설정 및 수정 메서드 추가
    추가대상 :  생성자 -> host.setRole(), 생성자 -> host.setTag(), addGuest(), removeGuest(), removeAllUsers()

--- src/main/java/Teemo/Teemo_backend/domain/Tag.java
+++ src/main/java/Teemo/Teemo_backend/domain/Tag.java
@@ -59,9 +59,33 @@ public class Tag {
         this.latitude = latitude;
         this.longitude = longitude;
         this.users.add(host);
-
+        host.setRole(Role.HOST); // 호스트 역할 부여
+        host.setTag(this); // 호스트 태그 등록
         createdAt = LocalDateTime.now(); // 태그 생성 시, 태그 생성 시간 저장 (서버기준)
         deletedAt = createdAt.plusHours(1); // 태그 삭제 시간은 생성 시간으로부터 1시간 뒤
+    }
+    public void addGuest(User user){
+        user.setTag(this);
+        user.setRole(Role.GUEST);
+        this.users.add(user);
+    }
+    public void removeGuest(User user){
+        user.unsetTag();
+        user.setRole(Role.VIEWER);
+
+        for(int i = 1; i<this.users.size(); i++)
+            if(users.get(i).equals(user)){
+                users.remove(i);
+                break;
+            }
+    }
+    public void removeAllUsers(){
+        int idx=0;
+        for(User user: this.users){
+            user.unsetTag();
+            user.setRole(Role.VIEWER);
+        }
+        users = new ArrayList<>();
     }
 
 }
