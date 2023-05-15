38a82af5ca4c7e4033ca82c272667202ade74277
yunshin <2younsin@gmail.com>
2023-05-16 08:15:06 +0900

    [Feat] MemberUpdateRequest class -> 기본 생성자, 필드초기화 생성자 추가

--- src/main/java/Teemo/Teemo_backend/domain/dtos/MemberUpdateRequest.java
+++ src/main/java/Teemo/Teemo_backend/domain/dtos/MemberUpdateRequest.java
@@ -1,7 +1,11 @@
 package Teemo.Teemo_backend.domain.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class MemberUpdateRequest {
     private Long id;
