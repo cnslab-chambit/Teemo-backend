1599b8301a6d2c4bbb3e74ced5332e10890ee6e4
yunshin <2younsin@gmail.com>
2023-05-16 08:16:50 +0900

    [Feat] TagSearchResponse class -> 기본 생성자, 필드초기화 생성자, getter() 추가

--- src/main/java/Teemo/Teemo_backend/domain/dtos/TagSearchResponse.java
+++ src/main/java/Teemo/Teemo_backend/domain/dtos/TagSearchResponse.java
@@ -1,16 +1,15 @@
 package Teemo.Teemo_backend.domain.dtos;
 
 import Teemo.Teemo_backend.domain.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-// tagId, title, maxNum, targetGender, upperAge, lowerAge, remainingTime, hostNickName, hostGender, hostAge
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class TagSearchResponse {
     Long tagId;
     Double latitude;
     Double longitude;
-
-    public TagSearchResponse(Long tagId, Double latitude, Double longitude){
-        this.tagId = tagId;
-        this.latitude = latitude;
-        this.longitude = longitude;
-    }
 }
