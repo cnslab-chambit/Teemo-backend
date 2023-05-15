9e6b35d822867c65dbc7cfc3af1a8f77d858869a
yunshin <2younsin@gmail.com>
2023-05-16 08:15:28 +0900

    [Feat] TagCreateRequest class -> 기본 생성자, 필드초기화 생성자 추가

--- src/main/java/Teemo/Teemo_backend/domain/dtos/TagCreateRequest.java
+++ src/main/java/Teemo/Teemo_backend/domain/dtos/TagCreateRequest.java
@@ -1,8 +1,12 @@
 package Teemo.Teemo_backend.domain.dtos;
 
 import Teemo.Teemo_backend.domain.Gender;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class TagCreateRequest {
     private Long memberId;
