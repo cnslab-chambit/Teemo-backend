9bec8616fb77ce17da03ebe3a0b123087f211bf4
yunshin <2younsin@gmail.com>
2023-05-18 17:50:12 +0900

    [Feat] ChatroomService interface -> method 추가

--- src/main/java/Teemo/Teemo_backend/service/ChatroomService.java
+++ src/main/java/Teemo/Teemo_backend/service/ChatroomService.java
@@ -1,4 +1,14 @@
 package Teemo.Teemo_backend.service;
 
+import Teemo.Teemo_backend.domain.Chat;
+import Teemo.Teemo_backend.domain.dtos.ChatroomCreateRequest;
+import Teemo.Teemo_backend.domain.dtos.ChatroomSearchResponse;
+
+import java.util.List;
+
 public interface ChatroomService {
+    ChatroomSearchResponse create(ChatroomCreateRequest request); // 채팅방 생성
+    List<ChatroomSearchResponse> search(Long memberId, Long tagId); // 참여 가능한 채팅방 검색
+    List<Chat> load(Long chatroomId);// 특정 채팅방 입장 ( = 채팅 내역 불러오기 )
+    void remove(Long memberId,Long chatroomId);// 채팅방 삭제
 }
