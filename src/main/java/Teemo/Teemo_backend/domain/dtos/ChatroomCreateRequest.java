d44685064add62b470e6d1541df1e69de57bdfdd
yunshin <2younsin@gmail.com>
2023-05-20 16:01:07 +0900

    [Feat] ChatroomCreateRequest class -> @NotNull 추가

--- src/main/java/Teemo/Teemo_backend/domain/dtos/ChatroomCreateRequest.java
+++ src/main/java/Teemo/Teemo_backend/domain/dtos/ChatroomCreateRequest.java
@@ -1,5 +1,6 @@
 package Teemo.Teemo_backend.domain.dtos;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,8 +9,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Getter
 public class ChatroomCreateRequest {
+    @NotNull
     private Long memberId;      // Guest 의 아이디
+    @NotNull
     private Long tagId;         // Tag 의 아이디
+    @NotNull
     private Double latitude;    // Guest 의 현재 위도
+    @NotNull
     private Double longitude;   // Guest 의 현재 경도
 }
