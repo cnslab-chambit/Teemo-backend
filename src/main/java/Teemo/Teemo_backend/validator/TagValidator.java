0edd13909fcd2949c7d382a3978b24fc8eead8b3
yunshin <2younsin@gmail.com>
2023-05-20 18:02:39 +0900

    [Feat] TagValidator interface 추가

---
+++ src/main/java/Teemo/Teemo_backend/validator/TagValidator.java
@@ -0,0 +1,16 @@
+package Teemo.Teemo_backend.validator;
+
+import Teemo.Teemo_backend.domain.Gender;
+import Teemo.Teemo_backend.domain.Tag;
+
+public interface TagValidator {
+    public boolean found(Object found);
+    boolean checkLatitude(Double latitude);
+    boolean checkLongitude(Double longitude);
+    boolean checkTitleLength(Integer titleLength);
+    boolean checkDetailLength(Integer detailLength) ;
+    boolean checkMaxNum(Integer maxNum);
+    boolean checkUpperAge(int upperAge, int memberAge);
+    boolean checkLowerAge(int lowerAge, int memberAge);
+    boolean checkTargetGender(Gender targetGender);
+}
