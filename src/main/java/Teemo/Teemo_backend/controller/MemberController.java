08ee70e2e573b9fa992d8e2f9b9fea0fc0a4c755
yunshin <2younsin@gmail.com>
2023-05-29 14:59:42 +0900

    [Feat] MemberController class 추가

---
+++ src/main/java/Teemo/Teemo_backend/controller/MemberController.java
@@ -0,0 +1,13 @@
+package Teemo.Teemo_backend.controller;
+
+import Teemo.Teemo_backend.domain.dtos.*;
+import org.springframework.http.ResponseEntity;
+
+public interface MemberController {
+    public ResponseEntity signup(MemberSignupRequest request);
+    public MemberFindResponse findMember(Long memberId);
+    public ResponseEntity updateMember(MemberUpdateRequest request);
+    public ResponseEntity deleteMember(Long memberId);
+    public MemberLoginResponse login(MemberLoginRequest request);
+    public ResponseEntity logout(Long memberId);
+}
