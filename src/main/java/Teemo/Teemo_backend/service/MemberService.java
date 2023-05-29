c34068d5f0b7ca223896d51528b9bb48cdc56204
yunshin <2younsin@gmail.com>
2023-05-29 15:03:34 +0900

    [Feat] MemberService interface -> login(),logout() 추가

--- src/main/java/Teemo/Teemo_backend/service/MemberService.java
+++ src/main/java/Teemo/Teemo_backend/service/MemberService.java
@@ -1,12 +1,15 @@
 package Teemo.Teemo_backend.service;
 
 import Teemo.Teemo_backend.domain.Member;
+import Teemo.Teemo_backend.domain.dtos.MemberLoginRequest;
 import Teemo.Teemo_backend.domain.dtos.MemberSignupRequest;
 import Teemo.Teemo_backend.domain.dtos.MemberUpdateRequest;
 
 public interface MemberService {
     void join(MemberSignupRequest request); // 회원가입
-    Member find(Long id);// 회원조회
+    Member find(Long memberId);// 회원조회
     void update(MemberUpdateRequest memberUpdateRequest);// 정보 수정
-    void remove(Long id);// 회원탈퇴
+    void remove(Long memberId);// 회원탈퇴
+    Member login(MemberLoginRequest request);// 로그인
+    void logout(Long memberId);// 로그아웃
 }
