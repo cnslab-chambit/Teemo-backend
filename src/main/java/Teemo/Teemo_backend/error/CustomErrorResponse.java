0f24f2ceae01c5d3795bebe639d51052d4fae03b
yunshin <2younsin@gmail.com>
2023-06-03 22:03:12 +0900

    [Feat] CustomErrorResponse class -> getter() 추가

--- src/main/java/Teemo/Teemo_backend/error/CustomErrorResponse.java
+++ src/main/java/Teemo/Teemo_backend/error/CustomErrorResponse.java
@@ -1,8 +1,10 @@
 package Teemo.Teemo_backend.error;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public class CustomErrorResponse {
     private String field;
     private String message;
