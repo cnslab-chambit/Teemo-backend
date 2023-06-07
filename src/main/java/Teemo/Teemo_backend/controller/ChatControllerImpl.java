5a2c92c2df38c4ff1bc7d3ed0542643024c3242b
yunshin <2younsin@gmail.com>
2023-06-07 12:36:26 +0900

    [Style] ChatControllerImpl class -> 로그 출력 코드 정리

--- src/main/java/Teemo/Teemo_backend/controller/ChatControllerImpl.java
+++ src/main/java/Teemo/Teemo_backend/controller/ChatControllerImpl.java
@@ -5,7 +5,6 @@ import Teemo.Teemo_backend.domain.dtos.LocationSendDTO;
 import Teemo.Teemo_backend.domain.dtos.MsgSendDTO;
 import Teemo.Teemo_backend.service.ChatService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -15,15 +14,13 @@ import java.time.LocalDateTime;
 
 @RestController
 @RequiredArgsConstructor
-@Slf4j
 public class ChatControllerImpl {
     private final ChatService chatService;
     private final SimpMessagingTemplate simpMessagingTemplate;
     @MessageMapping("/chatroom/{id}")
     public void chatting(@DestinationVariable("id") Long id, MsgPalette msgPalette) throws Exception{
         String msgType = msgPalette.getMsgType();
-        if(msgType.equals("CHAT")){ // 채팅하기
-            log.info("CHAT");
+            if(msgType.equals("CHAT")){ // 채팅하기
             String sender = msgPalette.getSender();
             String content = msgPalette.getContent();
             if(sender==null || sender==null) return;
@@ -33,11 +30,9 @@ public class ChatControllerImpl {
             chatService.saveToMemory(msgSendDto);
         }
         else if(msgType.equals("SAVE")){ // db에 저장하기
-            log.info("SAVE");
             chatService.saveToDB();
         }
         else if(msgType.equals("NAVIGATE")){ // 내 위치 전송
-            log.info("NAVIGATE");
             String latitude = msgPalette.getLatitude();
             String longitude = msgPalette.getLongitude();
             if(latitude.isEmpty() || longitude.isEmpty()) return;
