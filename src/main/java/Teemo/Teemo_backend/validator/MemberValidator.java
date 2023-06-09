23835640e6bc6a36fc3b61a304d117dee2f48ad7
yunshin <2younsin@gmail.com>
2023-06-09 21:24:51 +0900

    [Feat] MemberValidator interface -> 메서드 추가

--- src/main/java/Teemo/Teemo_backend/validator/MemberValidator.java
+++ src/main/java/Teemo/Teemo_backend/validator/MemberValidator.java
@@ -6,7 +6,12 @@ import Teemo.Teemo_backend.domain.Role;
 import java.time.LocalDate;
 
 public interface MemberValidator {
-    public boolean comparePassword(String password, String shouldBe);
+    boolean checkEmailLength(String email);
+    boolean checkPasswordLength(String password);
+    boolean checkBirthdayAssertion(String birthday);
+    boolean checkBirthdayExpression(String birthday);
+    boolean checkNicknameLength(String nickname);
+    boolean comparePassword(String password, String shouldBe);
     boolean checkDeadLine(LocalDate now, LocalDate deletedAt);
-    public boolean checkRole(Role found, Role shouldBe);
+    boolean checkRole(Role found, Role shouldBe);
 }
